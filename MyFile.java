e0f24045b7c55a81f6de3cd53d241a559c7c8b9d
micomee <104212987+micomee@users.noreply.github.com>
2022-04-23 17:58:33 +0800

    Update MyFile.java

--- MyFile.java
+++ MyFile.java
@@ -1,5 +1,6 @@
-package com.myfile.MyFile;
+package com.myfile;
 import java.io.*;
+import java.util.*;
 
 public class MyFile
 {
@@ -76,14 +77,14 @@ public static void updateFile(String text)
          e.printStackTrace();
       }
   }
-  public static String getRandomPeer(String file) {
-        String result = "";
-        String wg_interface = "";
-        String wg_peer = "";
+  public static String getRandomPeer(String fileName) {
+        String all ="";
+        boolean isInterface = false;
+        boolean isPeer = false;
+        /*
         try {
             FileReader fr = new FileReader(new File(file));
-            boolean isInterface = false;
-            boolean isPeer = false;
+            
             BufferedReader br = new BufferedReader(fr);
             String line = br.readLine();
             while (line != null) {
@@ -102,9 +103,68 @@ public static void updateFile(String text)
                 }
                 line = br.readLine(); 
 }
+	br.close();
+	fr.close();
     } catch (FileNotFoundException e){}
     catch (IOException e) {}
-    
+    */
+        String encoding = "UTF-8";
+        File file = new File(fileName);
+        Long filelength = file.length();
+        byte[] filecontent = new byte[filelength.intValue()];
+        try {
+            FileInputStream in = new FileInputStream(file);
+            in.read(filecontent);
+            in.close();
+        } catch (FileNotFoundException e) {} catch (IOException e) {}
+        try {
+            all = new String(filecontent, encoding); } catch (UnsupportedEncodingException e) {}
+        String split[] = all.split("\\n", 0);
+        ArrayList<String> wg_interface = new ArrayList<>();
+        ArrayList<String> wg_peer = new ArrayList<>();
+         List<Integer> mark = new ArrayList<>(); 
+        int count = 0;
+        for(int i=0;i<split.length;i++){
+            if(split[i].contains("Interface") ) {
+                    isInterface = true; }
+            if (split[i].contains("Peer") ) {
+            	count = count+1;
+                    isPeer = true;}
+              if(isInterface && !isPeer){
+              	wg_interface.add(split[i]); }
+              if (split[i].contains("Peer") ) {
+                    isInterface = false; 
+                    isPeer = true;}
+              if (isPeer) {
+              	wg_peer.add(split[i]); }
+        }
+        int index = (int)(Math.random()*(count));
+        
+        all = "";
+        for(int i=0;i<wg_interface.size();i++){
+        	all = all + wg_interface.get(i) + "\n"; }
+        
+        count=0;
+        
+        for(int i=0; i<wg_peer.size(); i++){
+        	if(wg_peer.get(i).contains("Peer") ){
+        	    mark.add(i);
+        	}
+        }
+        
+        int j = mark.get(index);
+        boolean complete = false;
+        
+        for(int i=j; i<wg_peer.size(); i++){
+        	if (wg_peer.get(i) == ""){ continue ;}
+            if(!complete){
+            	all = all + wg_peer.get(i) + "\n";   } 
+            if((i < wg_peer.size()-1 ) && wg_peer.get(i+1).contains("Peer") ){
+            	break ;
+            }
+        }
+      //  appendLog(all);
+     /*
     String str= wg_peer;
     String newStr="Peer";
     int count=0;
@@ -118,10 +178,18 @@ public static void updateFile(String text)
     
     int average = wg_peer.length() / len3 ;
     // wg_interface = wg_interface + String.valueOf(index) + wg_peer;
-  // wg_interface = wg_interface + String.valueOf(len1) + wg_peer.substring(index * average, (index+1) * average -1 ) + String.valueOf(average) ;
-  wg_interface = wg_interface + wg_peer.substring(index * average, (index+1) * average -1 ) ;
+  appendLog(wg_peer);
+  appendLog(wg_interface);
+  appendLog(String.valueOf(len1) );
+  appendLog(String.valueOf(len2) );
+  appendLog(String.valueOf(len3) );
+  appendLog(String.valueOf(index) );
+  appendLog(String.valueOf(average) );
+*/
+  //wg_interface = wg_interface + wg_peer.substring(index * average, (index+1) * average  ) ;
   
  // String ret  = "/storage/emulated/0/tmp.conf" ;
+
    String ret  = "/data/user/0/com.tunsafe.app/files/Config/tmp.conf" ;
    
   File logFile = new File(ret);
@@ -129,11 +197,12 @@ public static void updateFile(String text)
          logFile.delete();
          logFile.createNewFile();
          BufferedWriter buf = new BufferedWriter(new FileWriter(logFile, true)); 
-      buf.append(wg_interface);
+      buf.append(all);
       buf.newLine();
       buf.close();
       } 
       catch (IOException e){}
         return ret ;
+       // return all;
     }
 }
